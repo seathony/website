4325936f7529a4a5711aa8f58b73a383c69948cc
Sean H <seathony@gmail.com>
2015-06-30 20:14:16 -0400

    server files

--- server.rb
+++ server.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
+require 'httparty'
+require 'nokogiri'
+response = HTTParty.get('Http://finance.yahoo.com/q?s=AAPL')
+dom = Nokogiri::HTML(response.body)
+list_of_results =  dom.xpath("//span[@id='yfs_l84_aapl']")
+result = list_of_results.first
 
 get '/' do
-  "Hello World"
-end
-
-get '/blah' do
-  "Hello World"
+  "The current price of AAPL is $#{result.text}"
 end
